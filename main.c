ebd8f111ee2e2d990039d2a784d56b3e7a4267a4
beng <benjamin.gretic@gmail.com>
2025-05-20 00:26:15 +0200

    hello world

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "cub3d.h"
 
+bool	check_filename(char *filename)
+{
+	if (!ft_strnstr(filename, ".ber", ft_strlen(filename)))
+		return (false);
+	if ((*filename == '.' && *(filename + 1) == 'b')
+		|| (*filename == '.' && *(filename + 2) == '.'))
+		return (false);
+	return (true);
+}
+
 int	main(int ac, char **av)
 {
 	t_data	data;
