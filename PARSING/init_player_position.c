3c0de3fa8d5b7ca777ecff5e8e5b27023ba60458
Benjamin Gretic <bgretic@c3r8p7.42vienna.com>
2025-06-10 17:41:07 +0200

    raycasting done

--- PARSING/init_player_position.c
+++ PARSING/init_player_position.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_player_position.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jwolfram <jwolfram@student.42vienna.com>   +#+  +:+       +#+        */
+/*   By: bgretic <bgretic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 15:38:30 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/05/30 15:51:54 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/06/10 17:35:47 by bgretic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ void	init_player_position(t_data *data, int y, int x)
 	if (data->map[y][x] == 'N')
 	{
 		data->player.dir_y = -1;
-		data->player.plane_y = -0.66;
+		data->player.plane_x = 0.66;
 	}
 	else if (data->map[y][x] == 'S')
 	{
 		data->player.dir_y = 1;
-		data->player.plane_y = 0.66;
+		data->player.plane_x = -0.66;
 	}
 	else if (data->map[y][x] == 'E')
 	{
 		data->player.dir_x = 1;
-		data->player.plane_x = 0.66;
+		data->player.plane_y = 0.66;
 	}
 	else
 	{
 		data->player.dir_x = -1;
-		data->player.plane_x = -0.66;
+		data->player.plane_y = -0.66;
 	}
-	data->player.x = x;
-	data->player.y = y;
+	data->player.x = x + 0.5;
+	data->player.y = y + 0.5;
 }
--- RAYCASTING/dda_algorithm.c
+++ RAYCASTING/dda_algorithm.c
@@ -9,8 +9,8 @@ void    calculate_dda_variables(t_data *data, double window_x)
     data->dda.map_y = (int)data->player.y;
     data->dda.ray_direction_x = data->player.dir_x + data->player.plane_x * camera;
     data->dda.ray_direction_y = data->player.dir_y + data->player.plane_y * camera;
-    data->dda.delta_distance_x = fabs(1 / data->dda.ray_direction_x); //maybe fabs
-    data->dda.delta_distance_y = fabs(1 / data->dda.ray_direction_y); //maybe fabs
+    data->dda.delta_distance_x = fabs(1 / data->dda.ray_direction_x);
+    data->dda.delta_distance_y = fabs(1 / data->dda.ray_direction_y);
 }
 
 void    calculate_step_and_side_distance(t_data *data)
@@ -61,13 +61,12 @@ void    calculate_distance_to_wall(t_data *data)
     	    data->dda.map_y += data->dda.step_y;
     	    data->dda.side = 1;
     	}
-
-    	if (data->map[data->dda.map_x][data->dda.map_y] == '1')
+    	if (data->map[data->dda.map_y][data->dda.map_x] == '1')
 		{
         	hit = true;
 			if (data->dda.side == 0)
 				data->dda.distance_to_wall = (data->dda.map_x - data->player.x + (1 - data->dda.step_x) / 2) / data->dda.ray_direction_x;
-			else if (data->dda.side == 1)
+			else
 				data->dda.distance_to_wall = (data->dda.map_y - data->player.y + (1 - data->dda.step_y) / 2) / data->dda.ray_direction_y;
 		}
 	}
