d0f80107716b7a296fbb4cbd706e59b063efd15a
Julia Wolfram <julia.wolfram@gmx.at>
2025-06-23 17:01:08 +0200

    merge: latest

--- PLAYER_MOVEMENT/movement_init.c
+++ PLAYER_MOVEMENT/movement_init.c
@@ -6,15 +6,23 @@
 /*   By: jwolfram <jwolfram@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 17:45:55 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/06/20 16:14:17 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/06/23 16:57:39 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+static int	hook_close(t_data *data)
+{
+		mlx_loop_end(data->mlx.mlx_pointer);
+		clean_exit(data, NULL);
+		return (0);
+}
 
 void	movement_init(t_data *data)
 {
 	mlx_loop_hook(data->mlx.mlx_pointer, &hook_idle, data);	
 	mlx_hook(data->mlx.mlx_window, 02, (1L << 0), &hook_key_press, data);
 	mlx_hook(data->mlx.mlx_window, 03, (1L << 1), &hook_key_release, data);
+	mlx_hook(data->mlx.mlx_window, 17, 0, &hook_close, data);
 }
